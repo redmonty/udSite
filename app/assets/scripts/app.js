7cea5fdec92f7db56070a0785d7e80ba892469b0
Redmonty <putiareddevil@gmail.com>
2017-09-24 17:36:27 +0300

    Completed links matching

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,9 +1,13 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 const mobileMenu = new MobileMenu();
+
 new RevealOnScroll($(".feature-item"),"85%");
 new RevealOnScroll($(".testimonial"), "60%");
+
+var stickyHeader = new StickyHeader();
 
 
